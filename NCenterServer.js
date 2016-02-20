915e354528bb42f7b4164ddd7b7ec17604e9f753
Ubuntu <ubuntu@ip-10-183-143-31.ec2.internal>
2016-02-20 03:53:43 +0000

    Initial commit from server

---
+++ NCenterServer.js
@@ -0,0 +1,35 @@
+//This is a server to accept connections from a web app/iOS App
+var express = require('express');
+var app =  express();
+var mysql = require('mysql');
+var bodyParser = require('body-parser');
+
+var connection = mysql.createConnection({
+	host : 'localhost',
+	user : 'root',
+	password : 'password'
+});
+
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+
+app.get('/',function(req,res) {
+	res.sendfile(_dirname + '/index.html');
+});
+
+app.post('/questions',function(req,res) {
+	connection.query('INSERT INTO QUESTIONS SET ?',req.body,
+		function(err, result) {
+		if(err) throw err;
+		res.send('Question added to the database with ID: ' + result.insertID);
+
+		}	
+	);
+});
+
+
+
+app.listen(3000);
+//console.log("Express server Listening on port %d in %s mode", app.address().port,app.settings.env);
